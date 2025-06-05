c9c88b7ca2c28256226c61e7f1d21f0fb2a14bf8
verisoft_user <reut.ishay@verisoft.co>
2025-06-06 01:02:57 +0300

    second commit

---
+++ vars/printVariables.groovy
@@ -0,0 +1,34 @@
+def printParams(){
+    echo "params: ${params}"
+}
+
+def printEnv(){
+    echo "env: ${env.COMBINED}"
+    echo "env: ${env.STATIC_VALUE}"
+}
+
+def jenkinsEnvVariables(){
+    echo "BUILD_NUMBER: ${env.BUILD_NUMBER}"
+    echo "BUILD_ID: ${env.BUILD_ID}"
+    echo "BUILD_URL: ${env.BUILD_URL}"
+    echo "JOB_NAME: ${env.JOB_NAME}"
+    echo "JOB_BASE_NAME: ${env.JOB_BASE_NAME}"
+    echo "BUILD_TAG: ${env.BUILD_TAG}"
+    echo "EXECUTOR_NUMBER: ${env.EXECUTOR_NUMBER}"
+    echo "NODE_NAME: ${env.NODE_NAME}"
+    echo "WORKSPACE: ${env.WORKSPACE}"
+    echo "JENKINS_HOME: ${env.JENKINS_HOME}"
+    echo "JENKINS_URL: ${env.JENKINS_URL}"
+    echo "GIT_COMMIT: ${env.GIT_COMMIT}"
+    echo "GIT_BRANCH: ${env.GIT_BRANCH}"
+}
+
+def buildVariable(){
+    echo "Display Name: ${currentBuild.displayName}"
+    echo "Description: ${currentBuild.description}"
+    echo "Build Number: ${currentBuild.number}"
+    echo "Result: ${currentBuild.result}"
+    echo "Start Time: ${currentBuild.startTimeInMillis}"
+    echo "Duration: ${currentBuild.duration}"
+    echo "URL: ${currentBuild.absoluteUrl}"
+}
