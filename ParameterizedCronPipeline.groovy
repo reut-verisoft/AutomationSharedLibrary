75f3ec6ae99b57db33b7d618398eaad80d4a9e09
verisoft_user <reut.ishay@verisoft.co>
2025-06-06 00:46:58 +0300

    first commit

---
+++ ParameterizedCronPipeline.groovy
@@ -0,0 +1,28 @@
+@Library("shared-library") _
+pipeline {
+    agent { label 'verisoft-2' }
+
+    parameters {
+        string(name: 'NAME', defaultValue: 'Reut', description: 'Enter your name')
+        booleanParam(name: 'DEBUG_MODE', defaultValue: false, description: 'Enable debug mode?')
+        choice(name: 'ENVIRONMENT', choices: ['DEV', 'QA', 'PROD'], description: 'Choose environment')
+    }
+    stages {
+        stage('Functions') {
+            steps {
+                script {
+                    properties([
+                            pipelineTriggers([
+                                    parameterizedCron("TZ=Asia/Jerusalem\n07 22 * * *%NAME=Gila;DEBUG_MODE=true\n08 22 * * *%ENVIRONMENT=PROD;DEBUG_MODE=true"
+                                    )
+                            ])
+                    ])
+                    echo "params: ${params}"
+                }
+            }
+        }
+    }
+}
+
+
+
